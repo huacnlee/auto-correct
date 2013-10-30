858d632f65e2416704e746c1ea699a102acfc0ee
Jason Lee <huacnlee@gmail.com>
2013-10-30 22:55:41 +0800

    Update Test case.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
-source 'http://ruby.taobao.org'
+source 'https://rubygems.org'
 gemspec
 gem 'rspec'
--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -2,71 +2,111 @@
 require "auto-space"
 
 describe "AutoSpace" do
-  it "should work with simple" do
-    "部署到heroku有问题网页不能显示".auto_space!.should == "部署到 heroku 有问题网页不能显示"
-  end
-  
-  it "should with () or []" do
-    "[北京]美企聘site/web大型应用开发高手-Ruby（Java/PHP/Python也可）".auto_space!.should == "[北京]美企聘 site/web 大型应用开发高手-Ruby（Java/PHP/Python 也可）"      
-    "[成都](团800)招聘Rails工程师".auto_space!.should == "[成都](团 800)招聘 Rails 工程师"
-  end
-  
-  it "should with . !" do
-    "Teahour.fm第18期发布".auto_space!.should == "Teahour.fm 第 18 期发布"
-    "Yes!升级到了Rails 4".auto_space!.should == "Yes! 升级到了 Rails 4"
-    "delete!方法是做什么的".auto_space!.should == "delete! 方法是做什么的"
-    "到了!升级到了Rails 4".auto_space!.should == "到了! 升级到了 Rails 4"
-  end
-  
-  it "should with URL" do
-    "http://sourceforge.net/解禁了".auto_space!.should == "http://sourceforge.net/ 解禁了"
-  end
-  
-  it "should with #" do
-    "个人信息显示公开记事本,记事本显示阅读次数#149".auto_space!.should == "个人信息显示公开记事本,记事本显示阅读次数 #149"      
-  end
-  
-  it "should with @" do
-    "里面用@foo符号的话后面的变量名会被替换成userN".auto_space!.should == "里面用 @foo 符号的话后面的变量名会被替换成 userN"
-  end
-  
-  it 'should with \ /' do
-    "我/解禁了".auto_space!.should == "我 / 解禁了"
-    "WWDC上讲到的Objective C/LLVM改进".auto_space!.should == "WWDC 上讲到的 Objective C/LLVM 改进"
+  let(:text) { "" }
+  subject { text.auto_space! }
+  context 'simple Chinese and English' do
+    let(:text) { "部署到heroku有问题网页不能显示" }
+    it { should eq "部署到 heroku 有问题网页不能显示" }
   end
 
-  it "should with number" do
-    "在Ubuntu11.10 64位系统安装newrelic出错".auto_space!.should == "在 Ubuntu11.10 64 位系统安装 newrelic 出错"
-    "升级了10.9 附遇到的bug".auto_space!.should == "升级了 10.9 附遇到的 bug"
-    "喜欢暗黑2却对D3不满意的可以看看这个。。".auto_space!.should == "喜欢暗黑 2 却对 D3 不满意的可以看看这个。。"
-    "在做ROR 3.2 Tutorial第Chapter 9.4.2遇到一个问题求助！".auto_space!.should == "在做 ROR 3.2 Tutorial 第 Chapter 9.4.2 遇到一个问题求助！"
+  context 'when have []' do
+    let(:text) { "[北京]美企聘site/web大型应用开发高手-Ruby" }
+    it { should eq "[北京]美企聘 site/web 大型应用开发高手-Ruby" }
+  end
+
+  context 'when have ()' do
+    let(:text) { "[成都](团800)招聘Rails工程师" }
+    it { should eq "[成都](团 800)招聘 Rails 工程师" }
+  end
+
+  context 'when have .' do
+    let(:text) { "Teahour.fm第18期发布" }
+    it { should eq "Teahour.fm 第 18 期发布" }
+  end
+
+  context 'when have !' do
+    context 'neer English' do
+      let(:text) { "Yes!升级到了Rails 4" }
+      it { should eq "Yes! 升级到了 Rails 4" }
+    end
+    context 'near Chinese' do
+      let(:text) { "到了!升级到了Rails 4" }
+      it { should eq "到了! 升级到了 Rails 4" }
+    end
+  end
+
+  context 'when have URL' do
+    let(:text) { "http://sourceforge.net/解禁了" }
+    it { should eq "http://sourceforge.net/ 解禁了" }
+  end
+
+  context 'when have #' do
+    let(:text) { "个人信息显示公开记事本,记事本显示阅读次数#149" }
+    it { should eq "个人信息显示公开记事本,记事本显示阅读次数 #149" }
+  end
+
+  context 'when have @' do
+    let(:text) { "里面用@foo符号的话后面的变量名会被替换成userN" }
+    it { should eq "里面用 @foo 符号的话后面的变量名会被替换成 userN" }
+  end
+
+  context 'when have /' do
+    let(:text) { "WWDC上讲到的Objective C/LLVM改进" }
+    it { should eq "WWDC 上讲到的 Objective C/LLVM 改进" }
+  end
+
+  context 'when have numbers' do
+    context 'near the English word' do
+      let(:text) { "在Ubuntu11.10 64位系统安装newrelic出错" }
+      it { should eq "在 Ubuntu11.10 64 位系统安装 newrelic 出错" }
+    end
+    context 'near the Chinese chars' do
+      let(:text) { "升级了10.9 附遇到的bug" }
+      it { should eq "升级了 10.9 附遇到的 bug" }
+    end
+    context 'complex1' do
+      let(:text) { "喜欢暗黑2却对D3不满意的可以看看这个。。" }
+      it { should eq "喜欢暗黑 2 却对 D3 不满意的可以看看这个。。" }
+    end
+    context 'complex2' do
+      let(:text) { "在做ROR 3.2 Tutorial第Chapter 9.4.2遇到一个问题求助！" }
+      it { should eq "在做 ROR 3.2 Tutorial 第 Chapter 9.4.2 遇到一个问题求助！" }
+    end
+  end
+
+  context 'other cases' do
+    it {
+      "创建一篇article，但是却爆了ActionDispatch::Cookies::CookieOverflow的异常".auto_space!.should == "创建一篇 article，但是却爆了 ActionDispatch::Cookies::CookieOverflow 的异常"
+    }
+    it {
+      "Mac安装软件新方法：Homebrew-cask".auto_space!.should == "Mac 安装软件新方法：Homebrew-cask"
+    }
+    it {
+      "Mac安装软件新方法: Homebrew-cask".auto_space!.should == "Mac 安装软件新方法: Homebrew-cask"
+    }
+    it {
+      "Gitlab怎么集成GitlabCI.".auto_space!.should == "Gitlab 怎么集成 GitlabCI."
+    }
+  end
+
+  context 'when 年月日' do
+    it { "5天的活动".auto_space!.should == "5 天的活动" }
+    it { "我10岁的时候".auto_space!.should == "我 10 岁的时候" }
+    it { "再过3天我们就去".auto_space!.should == "再过 3 天我们就去" }
+    it { "于3月10日开始".auto_space!.should == "于 3月10日 开始" }
+    it { "于3月开始".auto_space!.should == "于 3月 开始" }
+    it { "于2009年开始".auto_space!.should == "于 2009年 开始" }
+    it { "2013年3月10日-Ruby Saturday活动召集".auto_space!.should == "2013年3月10日-Ruby Saturday 活动召集" }
+    it { "2013年12月22号开始出发".auto_space!.should == "2013年12月22号开始出发" }
+    it { "12月22号开始出发".auto_space!.should == "12月22号开始出发" }
+    it { "22号开始出发".auto_space!.should == "22号开始出发" }
   end
   
-  it "should with other cases" do
-    "创建一篇article，但是却爆了ActionDispatch::Cookies::CookieOverflow的异常".auto_space!.should == "创建一篇 article，但是却爆了 ActionDispatch::Cookies::CookieOverflow 的异常"
-    "Mac安装软件新方法：Homebrew-cask".auto_space!.should == "Mac 安装软件新方法：Homebrew-cask"
-    "Mac安装软件新方法: Homebrew-cask".auto_space!.should == "Mac 安装软件新方法: Homebrew-cask"
-    "Gitlab怎么集成GitlabCI.".auto_space!.should == "Gitlab 怎么集成 GitlabCI."
-  end
-  
-  it "should with 年月日" do
-    "5天的活动".auto_space!.should == "5 天的活动"
-    "我10岁的时候".auto_space!.should == "我 10 岁的时候"
-    "再过3天我们就去".auto_space!.should == "再过 3 天我们就去"
-    "于3月10日开始".auto_space!.should == "于 3月10日 开始"
-    "于3月开始".auto_space!.should == "于 3月 开始"
-    "于2009年开始".auto_space!.should == "于 2009年 开始"
-    "2013年3月10日-Ruby Saturday活动召集".auto_space!.should == "2013年3月10日-Ruby Saturday 活动召集"
-    "2013年12月22号开始出发".auto_space!.should == "2013年12月22号开始出发"
-    "12月22号开始出发".auto_space!.should == "12月22号开始出发"
-    "22号开始出发".auto_space!.should == "22号开始出发"
-  end
-  
-  it "should with 日XX/年XX/月XX/号XX" do
-    "五号的人物".auto_space!.should == "五号的人物"
-    "本月的活动".auto_space!.should == "本月的活动"
-    "今年就会出来".auto_space!.should == "今年就会出来"
-    "在日本比linode还便宜一半的服务器啊".auto_space!.should == "在日本比 linode 还便宜一半的服务器啊"
-    "明日大家都在这里".auto_space!.should == "明日大家都在这里"
+  context 'when 日XX/年XX/月XX/号XX' do
+    it { "五号的人物".auto_space!.should == "五号的人物" }
+    it { "本月的活动".auto_space!.should == "本月的活动" }
+    it { "今年就会出来".auto_space!.should == "今年就会出来" }
+    it { "在日本比linode还便宜一半的服务器啊".auto_space!.should == "在日本比 linode 还便宜一半的服务器啊" }
+    it { "明日大家都在这里".auto_space!.should == "明日大家都在这里" }
   end
 end
